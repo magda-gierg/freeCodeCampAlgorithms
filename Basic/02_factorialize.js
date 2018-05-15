fbe11947cf2273e9016fb7752eef4193dd408a03
magda-gierg <mjgiergiczny@gmail.com>
2018-05-16 09:51:22 +1200

    refactor factorialize using recursion

--- Basic/02_factorialize.js
+++ Basic/02_factorialize.js
@@ -1,16 +1,26 @@
 function factorialize(num) {
-  if (num === 0) {
-    return 1;
+  if (num ===1) {
+    return 1
   } else {
-    var arr = []
-    for (var i = 1; i <= num; i++) {
-      arr.push(i)
-    }
-
-    return arr.reduce(function(a,b) {  /* a-accumulator and b-next value in the array   */
-      return a*b
-    })
+    return num * factorialize(num-1)  // I'm using recursion - function calls itself
   }
 }
+
+
+
+// function factorialize(num) {
+//   if (num === 0) {
+//     return 1;
+//   } else {
+//     var arr = []
+//     for (var i = 1; i <= num; i++) {
+//       arr.push(i)
+//     }
+//
+//     return arr.reduce(function(a,b) {  /* a-accumulator and b-next value in the array   */
+//       return a*b
+//     })
+//   }
+// }
 
 console.log(factorialize(5))
