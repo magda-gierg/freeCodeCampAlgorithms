7e86db918ba3b1273cd08fafbe565d620d9d8292
magda-gierg <mjgiergiczny@gmail.com>
2018-05-15 20:49:47 +1200

    refactor factorialize.js

--- Basic/02_factorialize.js
+++ Basic/02_factorialize.js
@@ -1,13 +1,15 @@
 function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    var arr = []
+    for (var i = 1; i <= num; i++) {
+      arr.push(i)
+    }
 
-  var arr = []
-  for (var i = 1; i <= num; i++) {
-    arr.push(i)
+    return arr.reduce(function(a,b) {  /* a-accumulator and b-next value in the array   */
+      return a*b
+    })
   }
-
-  return arr.reduce(function(a,b) {  /* a-accumulator and b-next value in the array   */
-    return a*b
-  })
-}
 
 console.log(factorialize(5))
