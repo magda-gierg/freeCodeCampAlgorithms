565c49241791a974e21620234f33206c4e2e0265
magda-gierg <mjgiergiczny@gmail.com>
2018-05-15 13:51:46 +1200

    repeat the string is working

---
+++ Basic/08_repeat_string.js
@@ -0,0 +1,16 @@
+function repeatStringNumTimes(str, num) {
+  if (num > 0) {
+    return str.repeat(num)
+  } else {
+    return ''
+  }
+}
+
+/*
+function repeatStringNumTimes(str, num) {
+return num > 0 ? str.repeat(num) : ""
+}
+*/
+
+
+console.log(("abc", 3))
