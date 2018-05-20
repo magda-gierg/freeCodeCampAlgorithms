2895380d670c58389a5af7284b2a27fe10adc313
magda-gierg <mjgiergiczny@gmail.com>
2018-05-21 09:30:58 +1200

    sum all numbers done

---
+++ Intermediate/01_sum_numbers.js
@@ -0,0 +1,12 @@
+function sumAll(arr) {
+  var min = Math.min(arr[0], arr[1])
+  var max = Math.max(arr[0], arr[1])
+  sum = 0
+
+  for (var i = min; i <= max; i++) {
+    sum += i
+  }
+  return sum
+}
+
+console.log(sumAll([1, 4]))
